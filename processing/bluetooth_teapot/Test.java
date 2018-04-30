ed848413ff6587dce07bc219d6ed4eb903930b10
Gary Rendle <gary.444@hotmail.co.uk>
2018-04-30 19:39:39 +0200

    added java files

---
+++ processing/bluetooth_teapot/Test.java
@@ -0,0 +1,14 @@
+public class Test {
+
+    private int factor;
+
+    public Test(int test_factor){
+
+        factor = test_factor;
+    }
+
+    public int multiply(int num){
+
+        return num * factor;
+    }
+}
